8b8a41071efc9ac97b8db0db756f9c359d097160
Skorgum <skorgum@gmail.com>
2025-11-04 22:49:45 -0500

    60 FPS

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,7 +15,8 @@ def main():
                 return
         screen.fill(("black"))
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000  # Convert to seconds
+        print(dt)
 
 if __name__ == "__main__":
     main()
